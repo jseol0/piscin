5823202b7f27e2244d9dcef0a5102c59e7eb7177
jseol0 <wodb0927@gmail.com>
2021-02-23 02:46:35 +0900

    02230226

---
+++ C02/ex03/ft_str_is_numeric.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_numeric.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jseol <jseol@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/22 23:43:02 by jseol             #+#    #+#             */
+/*   Updated: 2021/02/23 00:42:23 by jseol            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_str_is_numeric(char *str)
+{
+	int i;
+
+	if (str[0] == '\0')
+		return (1);
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (!(str[i] >= '0' && str[i] <= '9'))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	main(void)
+{
+	char str[] = "1";
+	printf("%d", ft_str_is_numeric(str));
+}
---
+++ C02/ex04/ft_str_is_lowercase.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_lowercase.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jseol <jseol@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/23 00:52:30 by jseol             #+#    #+#             */
+/*   Updated: 2021/02/23 01:05:02 by jseol            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_str_is_lowercase(char *str)
+{
+	int i;
+
+	if (str[0] == '\0')
+		return (1);
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (!(str[i] >= 'a' && str[i] <= 'z'))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	main(void)
+{
+	char str[] = "1";
+	printf("%d", ft_str_is_lowercase(str));
+}
